daf206a19f92f62381b70682b1d00968852707af
Channu17 <channudambal2004@gmail.com>
2025-05-20 18:24:03 +0530

    created the middelwares

--- project1/app.js
+++ project1/app.js
@@ -3,19 +3,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 import connectDB from "./database/mongodb.js";
-
+import cookieParser from "cookie-parser";
 
 import  userRouter from "./routes/user.routes.js";
 import  authRouter from "./routes/auth.routes.js";
 import  subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser())
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
-
+app.use(errorMiddleware);
 
 app.get("/", (req, res)=>{
     res.send({ body: "welcome to the subscription Tracker API" });
